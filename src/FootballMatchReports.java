538cc2478f6638df41042619032b41391d4cb99e
paulopassos0788 <paulo.passos1@gmail.com>
2025-01-31 21:56:26 -0300

    Relatórios de partidas de futebol

---
+++ src/FootballMatchReports.java
@@ -0,0 +1,22 @@
+public class FootballMatchReports {
+
+    public static void main(String[] args) {
+        System.out.println(onField(4));
+    }
+
+    public static String onField(int shirtNum) {
+        return switch (shirtNum) {
+            case 1 -> "goalie";
+            case 2 -> "left back";
+            case 3, 4 -> "center back";
+            case 5 -> "right back";
+            case 6, 7, 8 -> "midfielder";
+            case 9 -> "left wing";
+            case 10 -> "striker";
+            case 11 -> "right wing";
+            default -> throw new IllegalArgumentException("Throw IllegalArgumentException");
+        };
+
+    }
+
+}
